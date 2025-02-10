2187dbde0fa5593e075dd3c12db206b2b39f1bbb
franzgabriel <franzgabriel11@hotmail.de>
2025-02-10 17:30:20 +0100

    initial$

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Jetzt dreihemer eine dure!</Text>
       <StatusBar style="auto" />
     </View>
   );
